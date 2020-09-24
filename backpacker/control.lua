3fd130aa9e559d5db186de4eccd87d4d16c7673b
miZyind <mizyind@gmail.com>
2020-09-25 00:39:50 +0700

    feat: update backpacker parameters

--- backpacker/control.lua
+++ backpacker/control.lua
@@ -1,13 +1,14 @@
 local main = {}
 
 function main.init()
-  local crafting_speed_modifier = 10 -- 10x speed
   local distance_bonus = 125
   local inventory_slots_bonus = 60
 
-  game.forces["player"].manual_crafting_speed_modifier = crafting_speed_modifier
+  game.forces["player"].manual_crafting_speed_modifier = 1000
+  game.forces["player"].manual_mining_speed_modifier = 1000
   game.forces["player"].character_build_distance_bonus = distance_bonus
   game.forces["player"].character_reach_distance_bonus = distance_bonus
+  game.forces["player"].character_resource_reach_distance_bonus = distance_bonus
   game.forces["player"].character_inventory_slots_bonus = inventory_slots_bonus
 end
 
