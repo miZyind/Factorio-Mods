6d1eab735960012b5e6956c822507a0e3ca70512
miZyind <mizyind@gmail.com>
2021-10-03 13:57:49 +0700

    feat: update backpacker settings

--- backpacker/control.lua
+++ backpacker/control.lua
@@ -1,11 +1,12 @@
 local main = {}
 
 function main.init()
-  local distance_bonus = 125
-  local inventory_slots_bonus = 60
+  local speed_modifier = 1000
+  local distance_bonus = 0
+  local inventory_slots_bonus = 50
 
-  game.forces["player"].manual_crafting_speed_modifier = 1000
-  game.forces["player"].manual_mining_speed_modifier = 1000
+  game.forces["player"].manual_crafting_speed_modifier = speed_modifier
+  game.forces["player"].manual_mining_speed_modifier = speed_modifier
   game.forces["player"].character_build_distance_bonus = distance_bonus
   game.forces["player"].character_reach_distance_bonus = distance_bonus
   game.forces["player"].character_resource_reach_distance_bonus = distance_bonus
